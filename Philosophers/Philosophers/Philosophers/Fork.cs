af83e83bbde9d46a88f344f03e687dd57eb7bcc2
Anatoly <anatolypanushkin@mail.ru>
2021-11-27 15:32:12 +0300

    Init

---
+++ Philosophers/Philosophers/Philosophers/Fork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Philosophers
+{
+    public class Fork
+    {
+        private static int _idStatic=0;
+        public int IdFork { get; set; }
+        public bool Condition { get; set; }
+
+        public Fork()
+        {
+            IdFork = _idStatic++;
+            Condition = false;
+        }
+
+        public void TakeFork()
+        {
+            Condition = true;
+        }
+
+        public void PutFork()
+        {
+            Condition = false;
+        }
+        
+    }
+}
---
+++ Philosophers/Philosophers/Philosophers/Philosophers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Philosophers
+{
+    public class Philosophers
+    {
+        public Philosophers()
+        {
+            Thread thread = new Thread(new ThreadStart(CreatePhilosopher));
+        }
+
+        public void PhilosopherEating(Fork fork1,Fork fork2)
+        {
+            if (fork1.Condition == false || fork2.Condition == false)
+            {
+                
+            }
+        }
+
+        private void CreatePhilosopher()
+        {
+            Console.WriteLine($"Создан философ с Id - {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
