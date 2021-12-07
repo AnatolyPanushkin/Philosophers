64a816a289549307a7c7526b19b7c90e6a48ff68
Anatoly <anatolypanushkin@mail.ru>
2021-12-07 06:30:32 +0300

    Complete version!

--- Philosophers/Philosophers/Philosophers/Philosophers.cs
+++ Philosophers/Philosophers/Philosophers/Philosophers.cs
@@ -1,26 +1,82 @@
 ﻿using System;
+using System.Collections;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Philosophers
 {
-    public class Philosophers
+    public class Philosophers:IEnumerable
     {
+        private Mutex _philosopherMustWait = new Mutex();
+        public Thread _thread;
         public Philosophers()
         {
-            Thread thread = new Thread(new ThreadStart(CreatePhilosopher));
+            _thread = new Thread(CreatePhilosopher);
+            _thread.Start();
+        }
+        public Task PhilosopherThinking(Fork fork1,Fork fork2)
+        {
+            Thread.Sleep(2000);
+
+            Console.WriteLine($"Философ - {_thread.Name}  думает");
+            
+            PhilosopherEating(fork1,fork2);
+            
+            return Task.CompletedTask;
         }
 
-        public void PhilosopherEating(Fork fork1,Fork fork2)
+        public Task PhilosopherEating(Fork fork1, Fork fork2)
         {
-            if (fork1.Condition == false || fork2.Condition == false)
+            _philosopherMustWait.WaitOne();
+            if (fork2.Condition == false & fork1.Condition == false)
             {
+                fork1.Condition = true;
+                fork2.Condition = true;
                 
+                Console.WriteLine($"Философ - {_thread.Name} кушает");
+                
+                Thread.Sleep(5000);
+                
+                Console.WriteLine($"Философ - {_thread.Name} поел");
+                
+                
+                fork1.Condition = false;
+                fork2.Condition = false;
+                _philosopherMustWait.ReleaseMutex();
+                PhilosopherThinking(fork1, fork2);
+            }
+            else
+            {
+                PhilosopherThinking(fork1,fork2);
+            }
+            return Task.CompletedTask;
+        }
+
+            
+        public async Task PhilosopherProcessAsync(Fork fork1,Fork fork2)
+        {
+            if (fork2.Condition == false & fork1.Condition == false)
+            {
+               Task v1= Task.Run(()=>PhilosopherEating(fork1,fork2));
+               //Thread.Sleep(10000);//Спросить
+               await v1;
+            }
+            else
+            {
+                await Task.Run(()=>PhilosopherThinking(fork1,fork2)); 
             }
         }
 
-        private void CreatePhilosopher()
+        
+        
+        private static void CreatePhilosopher()
         {
-            Console.WriteLine($"Создан философ с Id - {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"Создан философ");
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
         }
     }
 }
